4ce2dd76b187544e1344c2a10124d64ebbdca150
abdelouahedd <ennouriabdelouahed@gmail.com>
2020-08-10 15:06:06 +0100

    refactoring code of topProfiles

--- src/components/topProfils/TopProfils.js
+++ src/components/topProfils/TopProfils.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import envelop from "../../assets/images/envelop.png";
 import user_post from "../../assets/images/resources/us-pic.png";
-function TopProfiles(props) {
+
+function TopProfiles() {
+    const users = [
+        {
+            id: 1,
+            img: "../../assets/images/resources/us-pic.png",
+            name: "John Doe",
+            job: "Graphic Designer",
+
+        }, {
+            id: 2,
+            img: "../../assets/images/resources/us-pic.png",
+            name: "John Doe",
+            job: "Graphic Designer",
+
+        }, {
+            id: 3,
+            img: "../../assets/images/resources/us-pic.png",
+            name: "John Doe",
+            job: "Graphic Designer",
+
+        }, {
+            id: 4,
+            img: "../../assets/images/resources/us-pic.png",
+            name: "John Doe",
+            job: "Graphic Designer",
+
+        },
+    ]
     return (
         <div className="top-profiles">
             <div className="pf-hd">
@@ -9,84 +37,23 @@ function TopProfiles(props) {
                 <i className="la la-ellipsis-v"/>
             </div>
             <div className="profiles-slider">
-                <div className="user-profy">
-                    <img src={user_post} alt=""/>
-                    <h3>John Doe</h3>
-                    <span>Graphic Designer</span>
-                    <ul>
-                        <li><a href="#" title="" className="followw">Follow</a>
-                        </li>
-                        <li><a href="#" title="" className="envlp"><img
-                            src={envelop} alt=""/></a></li>
-                        <li><a href="#" title="" className="hire">hire</a></li>
-                    </ul>
-                    <a href="#" title="">View Profile</a>
-                </div>
-                <div className="user-profy">
-                    <img src={user_post} alt=""/>
-                    <h3>John Doe</h3>
-                    <span>Graphic Designer</span>
-                    <ul>
-                        <li><a href="#" title="" className="followw">Follow</a>
-                        </li>
-                        <li><a href="#" title="" className="envlp"><img
-                            src={envelop} alt=""/></a></li>
-                        <li><a href="#" title="" className="hire">hire</a></li>
-                    </ul>
-                    <a href="#" title="">View Profile</a>
-                </div>
-                <div className="user-profy">
-                    <img src={user_post} alt=""/>
-                    <h3>John Doe</h3>
-                    <span>Graphic Designer</span>
-                    <ul>
-                        <li><a href="#" title="" className="followw">Follow</a>
-                        </li>
-                        <li><a href="#" title="" className="envlp"><img
-                            src={envelop} alt=""/></a></li>
-                        <li><a href="#" title="" className="hire">hire</a></li>
-                    </ul>
-                    <a href="#" title="">View Profile</a>
-                </div>
-                <div className="user-profy">
-                    <img src={user_post} alt=""/>
-                    <h3>John Doe</h3>
-                    <span>Graphic Designer</span>
-                    <ul>
-                        <li><a href="#" title="" className="followw">Follow</a>
-                        </li>
-                        <li><a href="#" title="" className="envlp"><img
-                            src={envelop} alt=""/></a></li>
-                        <li><a href="#" title="" className="hire">hire</a></li>
-                    </ul>
-                    <a href="#" title="">View Profile</a>
-                </div>
-                <div className="user-profy">
-                    <img src={user_post} alt=""/>
-                    <h3>John Doe</h3>
-                    <span>Graphic Designer</span>
-                    <ul>
-                        <li><a href="#" title="" className="followw">Follow</a>
-                        </li>
-                        <li><a href="#" title="" className="envlp"><img
-                            src={envelop} alt=""/></a></li>
-                        <li><a href="#" title="" className="hire">hire</a></li>
-                    </ul>
-                    <a href="#" title="">View Profile</a>
-                </div>
-                <div className="user-profy">
-                    <img src={user_post} alt=""/>
-                    <h3>John Doe</h3>
-                    <span>Graphic Designer</span>
-                    <ul>
-                        <li><a href="#" title="" className="followw">Follow</a>
-                        </li>
-                        <li><a href="#" title="" className="envlp"><img
-                            src={envelop} alt=""/></a></li>
-                        <li><a href="#" title="" className="hire">hire</a></li>
-                    </ul>
-                    <a href="#" title="">View Profile</a>
-                </div>
+                {
+                    users.map(
+                        user =>
+                            <div className="user-profy" key={user.id}>
+                                <img src={user.img} alt=""/>
+                                <h3>{user.name}</h3>
+                                <span>{user.job}</span>
+                                <ul>
+                                    <li><a href="#" title="" className="followw">Follow</a>
+                                    </li>
+                                    <li><a href="#" title="" className="envlp"><img
+                                        src={envelop} alt=""/></a></li>
+                                    <li><a href="#" title="" className="hire">hire</a></li>
+                                </ul>
+                                <a href="#" title="">View Profile</a>
+                            </div>
+                    )}
             </div>
         </div>
     );
