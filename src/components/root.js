904e3104d72fa54fc19c54676bf929997046349f
abdelouahedd <ennouriabdelouahed@gmail.com>
2020-08-13 16:17:28 +0100

    sparate routes in file route.js

--- src/components/root.js
+++ src/components/root.js
@@ -4,20 +4,13 @@ import {Route, Switch} from "react-router-dom";
 import Home from "../components/home/home/Home";
 import Profile from "../components/profile/Profile";
 import '../routes/route.css'
+import CreateRoutes from '../routes/route'
 
 function Root(props) {
     return (
         <div className="wrapper">
             <NavBar/>
-            <Switch>
-                <Route path='/' component={Home} exact/>
-                <Route path='/companies/company' component={Home} exact/>
-                <Route path='/projects' component={Home} exact/>
-                <Route path='/profile' component={Profile} exact/>
-                <Route path='/files' component={Home} exact/>
-                <Route path='/messages' component={Home} exact/>
-                <Route path='/settings' component={Home} exact/>
-            </Switch>
+            <CreateRoutes/>
         </div>
     );
 }
---
+++ src/routes/route.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
+import {
+    Route,
+    Switch,
+    withRouter
+} from "react-router-dom";
+import Home from "../components/home/home/Home";
+import Profile from "../components/profile/Profile";
+import './route.css'
+
+const  AnimatedSwitch =  withRouter(({location}) => (
+    <TransitionGroup>
+        <CSSTransition key={location.key} classNames="slide" timeout={1000}>
+            <Switch location={location}>
+                <Route path='/' component={Home} exact/>
+                <Route path='/companies/company' component={Home} exact/>
+                <Route path='/projects' component={Home} exact/>
+                <Route path='/profile' component={Profile} exact/>
+                <Route path='/files' component={Home} exact/>
+                <Route path='/messages' component={Home} exact/>
+                <Route path='/settings' component={Home} exact/>
+            </Switch>
+        </CSSTransition>
+    </TransitionGroup>
+));
+
+const CreateRoutes = ()=>(
+    <Switch>
+        <Route path='/' component={Home} exact/>
+        <Route path='/companies/company' component={Home} exact/>
+        <Route path='/projects' component={Home} exact/>
+        <Route path='/profile' component={Profile} exact/>
+        <Route path='/files' component={Home} exact/>
+        <Route path='/messages' component={Home} exact/>
+        <Route path='/settings' component={Home} exact/>
+    </Switch>
+);
+export default CreateRoutes;
+
