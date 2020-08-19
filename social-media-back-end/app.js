9d5b3f12a25d78d44337b2f4c2d96b2afb5f0539
abdelouahedd <ennouriabdelouahed@gmail.com>
2020-08-19 16:24:54 +0100

    add middelware for not found route

--- social-media-back-end/app.js
+++ social-media-back-end/app.js
@@ -4,23 +4,42 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var mongo = require('mongoose');
+var morgan = require('morgan');
 require("dotenv").config();
 
 //Routes
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//middleware for /users    
 app.use('/users', usersRouter);
+
+//middleware ERROR
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 
 mongo.connect(process.env.MONGO_LOCAL,
     {
