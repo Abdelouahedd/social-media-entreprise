b4eb88a28ea508d9e978c257b2172a2d80d5d572
abdelouahedd <ennouriabdelouahed@gmail.com>
2020-08-20 20:06:22 +0100

    methode for updating and deleting a post

--- back-end/controllers/postController.js
+++ back-end/controllers/postController.js
@@ -1,6 +1,7 @@
 
 const { upload } = require('../helper/upload');
 const post = require('../models/post');
+const mongoose = require('mongoose')
 exports.createPost = async (req, res) => {
     try {
         const { sujet } = req.body;
@@ -18,6 +19,43 @@ exports.createPost = async (req, res) => {
             }
             res.status(200).send({ success: true, msg: `Post is created by succesfully`, post: post });
         })
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+}
+
+exports.updatePost = async (req, res) => {
+    try {
+        const { sujet } = req.body;
+
+        await post.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(req.params.id) },
+            {
+                $set: {
+                    sujet: sujet,
+                }
+            },
+            { new: true },
+            (err, post) => {
+                if (err) {
+                    res.status(500).json({ success: false, error: err.message });
+                }
+                res.send({ success: true, message: "Post uptaded by succesfully", post: post });
+            });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+}
+
+exports.deletePost = async (req, res) => {
+    try {
+
+        await post.findByIdAndDelete({ _id: mongoose.Types.ObjectId(req.params.id) },
+            (err) => {
+                if (err) {
+                    res.status(500).json({ success: false, error: err.message });
+                }
+                res.send({ success: true, message: "Post deleted by succesfully" });
+            });
     } catch (error) {
         res.status(500).json({ success: false, error: error.message });
     }
--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var { createPost } = require('../controllers/postController');
+var { createPost, updatePost, deletePost } = require('../controllers/postController');
 var { verifyToken } = require('../helper/helper');
 var { upload } = require('../helper/upload');
-router.post('/createPost', verifyToken, upload.single('photo_post'), createPost);
 
+router.post('/createPost', verifyToken, upload.single('photo_post'), createPost);
+router.put('/editPost/:id', verifyToken, updatePost);
+router.delete('/deletePost/:id', verifyToken, deletePost);
 // router.post('/', );
 
 module.exports = router;
