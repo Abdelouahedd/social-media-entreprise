6fbdf8bf3372d5ea1aaf79821783082ae18615d2
abdelouahedd <ennouriabdelouahed@gmail.com>
2020-08-22 17:00:07 +0100

    fix auth methode

--- back-end/app.js
+++ back-end/app.js
@@ -18,6 +18,10 @@ app.use(logger('dev'));
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- src/components/authentification/compnents/signIN.js
+++ src/components/authentification/compnents/signIN.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { ToastProvider, useToasts } from 'react-toast-notifications'
+import React, { useState } from 'react';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux'
+import { useToasts } from 'react-toast-notifications'
 import { useHistory } from 'react-router-dom';
 import { authStart, authSuccess, authFail } from '../../../redux/actions/authActions';
-import Axios from 'axios';
 import { URL } from '../../../redux/_helper/utility';
+import SpinnerLoad from '../../shared/spinner/SpinnerLoad';
+
 
 export default function SignIn() {
     const { addToast } = useToasts()
@@ -12,28 +13,40 @@ export default function SignIn() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState({ errorEmail: false, errorPass: false })
-    const authentification = useSelector(state => state.auth);
+    const authentification = useSelector(state => state.auth, shallowEqual);
     const dispatch = useDispatch();
     const history = useHistory();
 
     const auth = (user) => async (dispatch) => {
         dispatch(authStart());
-        Axios.post(`${URL}/users/signIN`, user).
-            then((response) => {
-                console.log(response);
-                dispatch(authSuccess(response.data.token, response.data.msg));
-                addToast(response.data.msg, { appearance: 'success', autoDismiss: true },)
-
-                console.log('GLOBAL STATE IS :', authentification)
-                history.push('/');
-            })
-            .catch(err => {
-                console.log(err);
-                addToast(err.message, { appearance: 'error', autoDismiss: true },)
-                dispatch(authFail(err));
-            });
-
+        fetch(`${URL}/users/signIN`, {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(async (res) => {
+            let response = await res.json();
+            console.log(response);
+            if (res.status === 200) {
+                setTimeout(() => {
+                    dispatch(authSuccess(response.token, response.msg));
+                    addToast(response.msg, { appearance: 'success', autoDismiss: true },);
+                    history.push('/');
+                }, 3000);
+            } else {
+                setTimeout(() => {
+                    dispatch(authFail(response.msg));
+                    addToast(response.msg, { appearance: 'error', autoDismiss: true },)
+                }, 3000);
+            }
+        }).catch(err => {
+            console.error(err);
+            dispatch(authFail(err));
+            alert('Error logging in please try again');
+        });
     }
+    
 
 
     const emailHandlerChanger = (event) => {
@@ -45,84 +58,98 @@ export default function SignIn() {
     }
     const passwordHandler = event => {
         let pass = event.target.value;
-        pass.length < 5 ?
+        pass.length < 6 ?
             setError({ ...error, errorPass: true }) :
             setError({ ...error, errorPass: false });
 
         setPassword(event.target.value);
     }
+
     const onSubmit = (e) => {
         e.preventDefault();
+        if (error.errorPass == true) {
+            return addToast("Min 6 caractere pour le mot de passe", { appearance: 'error', autoDismiss: true },)
+        }
+        if (error.errorEmail == true) {
+            return addToast("Entrer valide Email", { appearance: 'error', autoDismiss: true },)
+        }
         const user = {
             email: email,
             mot_pass: password
         }
-        console.log('BEFORE GLOBAL STATE IS :', authentification)
         dispatch(auth(user));
     }
-
+ 
     return (
-        <div className="sign_in_sec current" id="tab-1">
-            <h3>Sign in</h3>
+        <>
+            <div className="sign_in_sec current" id="tab-1">
+                <h3>Sign in</h3>
+                <form onSubmit={onSubmit} method="POST">
+                    <div className="row">
+                        <div className="col-lg-12 no-pdd">
+                            <div className="sn-field" >
+                                <input type="text"
+                                    name="email"
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={emailHandlerChanger}
+                                    style={{ borderColor: error.errorEmail ? "red" : " #e5e5e5" }}
 
-            <form onSubmit={onSubmit} method="POST">
-                <div className="row">
-                    <div className="col-lg-12 no-pdd">
-                        <div className="sn-field" >
-                            <input type="text"
-                                name="email"
-                                placeholder="Email"
-                                value={email}
-                                onChange={emailHandlerChanger}
-                                style={{ borderColor: error.errorEmail ? "red" : " #e5e5e5" }}
-
-                            />
-                            <i className="fa fa-user" /><br />
-                        </div>
-                    </div>
-                    <div className="col-lg-12 no-pdd">
-                        <div className="sn-field">
-                            <input type="password"
-                                name="password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={passwordHandler}
-                                style={{ borderColor: error.errorPass ? "red" : " #e5e5e5" }}
-                            />
-                            <i className="la la-lock" />
-                        </div>
-                    </div>
-                    <div className="col-lg-12 no-pdd">
-                        <div className="checky-sec">
-                            <div className="fgt-sec">
-                                <input type="checkbox" name="cc" id="c1" />
-                                <label htmlFor="c1">
-                                    <span />
-                                </label>
-                                <small>Remember me</small>
+                                />
+                                <i className="fa fa-user" /><br />
                             </div>
-                            <a href="#" title="">Forgot Password?</a>
+                        </div>
+                        <div className="col-lg-12 no-pdd">
+                            <div className="sn-field">
+                                <input type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={passwordHandler}
+                                    style={{ borderColor: error.errorPass ? "red" : " #e5e5e5" }}
+                                />
+                                <i className="la la-lock" />
+                            </div>
+                        </div>
+                        <div className="col-lg-12 no-pdd">
+                            <div className="checky-sec">
+                                <div className="fgt-sec">
+                                    <input type="checkbox" name="cc" id="c1" />
+                                    <label htmlFor="c1">
+                                        <span />
+                                    </label>
+                                    <small>Remember me</small>
+                                </div>
+                                <a href="#" title="">Forgot Password?</a>
+                            </div>
+                        </div>
+                        <div className="col-lg-12 no-pdd">
+                            {
+                                authentification.loading == true ?
+                                    <SpinnerLoad /> :
+                                    <button type="submit" value="submit">Sign in</button>
+                            }
+
                         </div>
                     </div>
-                    <div className="col-lg-12 no-pdd">
-                        <button type="submit" value="submit">Sign in</button>
-                    </div>
-                </div>
-            </form>
-            <div className="login-resources">
-                <h4>Login Via Social Account</h4>
-                <ul>
-                    <li>
-                        <a href="/" title="" className="fb">
-                            <i className="fa fa-facebook" />Login Via Facebook</a>
-                    </li>
-                    <li>
-                        <a href="/" title="" className="tw">
-                            <i className="fa fa-twitter" />Login Via Twitter</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+                </form>
 
+                <div className="login-resources">
+                    <h4>Login Via Social Account</h4>
+                    <ul>
+                        <li>
+                            <a href="/" title="" className="fb">
+                                <i className="fa fa-facebook" />Login Via Facebook</a>
+                        </li>
+                        <li>
+                            <a href="/" title="" className="tw">
+                                <i className="fa fa-twitter" />Login Via Twitter</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </>
     )
 }
