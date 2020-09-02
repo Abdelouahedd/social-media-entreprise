e7fd308a43ef590fe86f8e3f97e4197ab2913caa
AE <ennouriabdelouahed@gmail.com>
2020-09-03 00:50:32 +0100

    pass post object as props and display files in model

--- src/components/home/Post/Post.js
+++ src/components/home/Post/Post.js
@@ -1,13 +1,78 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './post.css'
 import Comment from "./comments/comment";
+import {Link} from "react-router-dom";
+import {Carousel, Modal} from "react-bootstrap";
+import Slider from "react-slick";
 
 function Post(props) {
+    const post = props;
+    const [show, setShow] = useState(false);
+    const handleShow = () => {
+        const root = document.querySelector('.wrapper');
+        const nav = document.querySelector('.iq-top-navbar');
+        root.classList.add('overlay');
+        nav.classList.add('active');
+        setShow(true)
+    }
+    const handleClose = () => {
+        const root = document.querySelector('.wrapper');
+        const nav = document.querySelector('.iq-top-navbar');
+        root.classList.remove('overlay');
+        nav.classList.remove('active');
+        setShow(false)
+    }
+    //State
     const [editOption, setEditOption] = useState(false);
     const [showComments, setShowComments] = useState(false);
     const [like, setLike] = useState(false);
 
+    //filter the URL from String
+    const urlify = (text) => {
+        let urlRegex = /(https?:\/\/[^\s]+)/g;
+        return text.replace(urlRegex, function (url) {
+            return `<a className="card-link" href=${url} target="_blank">${url}</a>`;
+        })
+    }
+    const filterContent = (file) => {
+        if (file.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+            return <img className="card-img-top rounded-0 " src={file} alt="Card image cap"/>
+        } else if (file.match(/\.(mp4)$/)) {
+            return <video className="card-img-top rounded-0" controls>
+                <source src={file}/>
+            </video>
+        }
+        return <img className="card-img-top rounded-0 " src={file} alt="Card image cap"/>
+    }
 
+    function Arrow(props) {
+        return (
+            <span className={props.styleArrow} onClick={props.onClick}/>
+        );
+    }
+
+    /*
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: false,
+            className: "profiles-slider",
+            variableWidth: true,
+            nextArrow: <Arrow styleArrow={"slick-nexti"}/>,
+            prevArrow: <Arrow styleArrow="slick-previous"/>
+        };*/
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        className: "center",
+    };
     return (
         <>
             <div className="card gedf-card">
@@ -15,12 +80,14 @@ function Post(props) {
                     <div className="d-flex justify-content-between align-items-center">
                         <div className="d-flex justify-content-between align-items-center">
                             <div className="mr-2">
-                                <img className="avatar rounded-circle" width="45" src="https://picsum.photos/50/50"
-                                     alt=""/>
+                                <img className="avatar rounded-circle" width="45"
+                                     src={post.user.photo_profil} alt=""/>
                             </div>
                             <div className="ml-2">
-                                <div className="h5 m-0">@LeeCross</div>
-                                <div className="h7 text-muted">Miracles Lee Cross</div>
+                                <Link to="/">
+                                    <div className="h5 m-0">{post.user.nom + " " + post.user.prenom}</div>
+                                </Link>
+                                {/*<div className="h7 text-muted">Miracles Lee Cross</div>*/}
                             </div>
                         </div>
                         <div className="ed-opts">
@@ -40,23 +107,36 @@ function Post(props) {
                     </div>
                 </div>
                 <div className="card-body">
-                    <div className="text-muted h7 mb-2"><i className="fa fa-clock-o"></i> Hace 40 min</div>
-
-                    <a className="card-link" href="#">
-                        <h5 className="card-title">Totam non adipisci hic! Possimus ducimus amet, dolores illo ipsum
-                            quos
-                            cum.</h5>
-                    </a>
-                    <p className="card-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sunt fugit reprehenderit
-                        consectetur exercitationem odio,
-                        quam nobis? Officiis, similique, harum voluptate, facilis voluptas pariatur dolorum tempora
-                        sapiente
-                        eius maxime quaerat.
-                        <a href="https://mega.nz/#!1J01nRIb!lMZ4B_DR2UWi9SRQK5TTzU1PmQpDtbZkMZjAIbv97hU"
-                           target="_blank">https://mega.nz/#!1J01nRIb!lMZ4B_DR2UWi9SRQK5TTzU1PmQpDtbZkMZjAIbv97hU</a>
-                    </p>
-                    <div className="photo">
+                    <div className="text-muted h7 mb-2"><i className="fa fa-clock-o"></i>{post.createdAt}</div>
+                    <p className="card-text" dangerouslySetInnerHTML={{__html: urlify(post.sujet)}}></p>
+                    {post.files.length > 3 ?
+                        (<div className="photo">
+                            {post.files.slice(0, 3).map((file, i) =>
+                                <div key={i} className="children">
+                                    {
+                                        i !== 2 ?
+                                            filterContent(file)
+                                            :
+                                            <div>
+                                                {filterContent(file)}
+                                                <div className="children_float" onClick={() => handleShow()}>
+                                                    +{post.files.length}
+                                                </div>
+                                            </div>
+                                    }
+                                </div>
+                            )}
+                        </div>)
+                        :
+                        (<div className="photo_two_children">
+                            {post.files.map((file, i) =>
+                                <div key={i} className="two_children">
+                                    {filterContent(file)}
+                                </div>
+                            )}
+                        </div>)
+                    }
+                    {/* <div className="photo">
                         <div className="children">
                             <img className="card-img-top rounded-0 " src="https://picsum.photos/320/250/?random"
                                  alt="Card image cap"/>
@@ -72,7 +152,7 @@ function Post(props) {
                                 +8
                             </div>
                         </div>
-                    </div>
+                    </div>*/}
                 </div>
                 <div className="card-footer bg-white border-1 p-0">
                     <div className="d-flex justify-content-between align-items-center my-1">
@@ -80,29 +160,54 @@ function Post(props) {
                             <button type="button" className="btn btn-fbook btn-block btn-sm"
                                     onClick={() => setLike(!like)}><i
                                 className={like ? "fa fa-heart" : "fa fa-heart-o"}
-                                aria-hidden="true"></i> Like
+                                aria-hidden="true"/> Like
                             </button>
                         </div>
                         <div className="col">
                             <button type="button" className="btn btn-fbook btn-block btn-sm"
                                     onClick={() => setShowComments(!showComments)}><i
                                 className="fa fa-comment-o"
-                                aria-hidden="true"></i> Commente
+                                aria-hidden="true"/> Commente
                             </button>
                         </div>
                         <div className="col">
-                            <button type="button" className="btn btn-fbook btn-block btn-sm"><i
-                                className="fa fa-share"
-                                aria-hidden="true"></i> Share
+                            <button type="button" className="btn btn-fbook btn-block btn-sm">
+                                <i className="fa fa-share" aria-hidden="true"/> Share
                             </button>
                         </div>
                     </div>
                 </div>
                 {/*Comment*/}
-                <Comment showComments={showComments}/>
+                <Comment commantaires={post.commantaires} showComments={showComments}/>
             </div>
+
+            <Modal
+                show={show}
+                size="lg"
+                onHide={() => handleClose()}
+                dialogClassName="modal-900w"
+                aria-labelledby="example-custom-modal-styling-title"
+            >
+                <Modal.Header closeButton>
+                     <Modal.Title id="example-custom-modal-styling-title">
+                         {post.sujet}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Carousel>
+                        {post.files.map((file, i) =>
+                            <Carousel.Item key={i} interval={100000}>
+                                <div className="d-block w-100">
+                                    {filterContent(file)}
+                                </div>
+                            </Carousel.Item>
+                        )}
+                    </Carousel>
+                </Modal.Body>
+            </Modal>
         </>
-    );
+    )
+        ;
 }
 
 export default Post;
--- src/components/home/home/Home.js
+++ src/components/home/home/Home.js
@@ -26,7 +26,25 @@ const Home = () => {
         postPopUp.classList.remove('active');
     }
 
-
+    const post = {
+        _id: "123",
+        sujet: "  Lorem ipsum dolor sit amet, https://abdelouahedd.github.io/profile/ consectetur adipisicing elit. Asperiores cumque delectus doloremque\n" +
+            "                        eligendi eos explicabo fugit illo inventore ipsa ipsum labore laborum, molestiae officia\n" +
+            "                        placeat, quaerat quia saepe sequi ut?",
+        files: [
+            "https://vod-progressive.akamaized.net/exp=1599072762~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2827%2F13%2F339135642%2F1349205911.mp4~hmac=19ce29fa910db58b0e28f1f78d59e04251460f1f4fb9a1bcf92b7ff00822692f/vimeo-prod-skyfire-std-us/01/2827/13/339135642/1349205911.mp4?filename=Pexels+Videos+2386458.mp4",
+            "https://picsum.photos/320/250/?random",
+            "https://vod-progressive.akamaized.net/exp=1599104393~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2562%2F14%2F362810087%2F1491690859.mp4~hmac=bb330efecb77cdb303d847386e47e14b429ecff02c245309daa7ca90a62bf6a6/vimeo-prod-skyfire-std-us/01/2562/14/362810087/1491690859.mp4?filename=video.mp4",
+            "https://image.freepik.com/psd-gratuit/sentez-vous-modele-publication-instagram-nature_23-2148607900.jpg"
+        ],
+        user: {
+            photo_profil: "http://localhost:4000/public/images/ded147df-3f52-46a2-bf17-dbd99b46f8fa-18.jpg",
+            nom: "Ennouri",
+            prenom: "Abdelouahed"
+        },
+        createdAt: "19/07/2020",
+        commantaires: []
+    }
     return (
         <>
             <main className="home-main">
@@ -57,8 +75,8 @@ const Home = () => {
                                         {/*start list view of posts*/}
                                         <div className="posts-section">
                                             <TopProfiles/>
-                                            <Post/>
-                                            <Post/>
+                                            <Post {...post}/>
+
                                             {/*  end list view of posts*/}
                                         </div>
                                     </div>
